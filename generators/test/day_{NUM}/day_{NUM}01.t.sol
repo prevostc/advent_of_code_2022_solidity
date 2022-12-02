2dbf20db861ca9b739190f5b74e593032420b384
Clément Prévost <prevostclement@gmail.com>
2022-12-02 16:40:39 +0100

    Add generator templates

---
+++ generators/test/day_{NUM}/day_{NUM}01.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/console2.sol";
+import "forge-std/Test.sol";
+import "../../src/day_{NUM}/Day{NUM}01.sol";
+
+contract Day{NUM}Test is Test {
+    Day{NUM}01 public day{NUM};
+
+    function setUp() public {
+        day{NUM} = new Day{NUM}01();
+    }
+
+    function test_Day{NUM}01_Example1() public {
+        string memory input = "";
+        emit log_string(input);
+        uint256 answer = day{NUM}.answer(input);
+        emit log_uint(answer);
+        assertEq(answer, 15);
+    }
+
+    function test_Day{NUM}01_MainChallenge() public {
+        string memory input = "";
+        //emit log_string(input);
+        uint256 answer = day{NUM}.answer(input);
+        console2.log("answer", answer);
+        assertEq(true, true); // no spoilers
+    }
+}
---
+++ generators/test/day_{NUM}/day_{NUM}02.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/console2.sol";
+import "forge-std/Test.sol";
+import "../../src/day_{NUM}/Day{NUM}02.sol";
+
+contract Day{NUM}Test is Test {
+    Day{NUM}02 public day{NUM};
+
+    function setUp() public {
+        day{NUM} = new Day{NUM}02();
+    }
+
+    function test_Day{NUM}02_Example1() public {
+        string memory input = "";
+        emit log_string(input);
+        uint256 answer = day{NUM}.answer(input);
+        emit log_uint(answer);
+        assertEq(answer, 15);
+    }
+
+    function test_Day{NUM}02_MainChallenge() public {
+        string memory input = "";
+        //emit log_string(input);
+        uint256 answer = day{NUM}.answer(input);
+        console2.log("answer", answer);
+        assertEq(true, true); // no spoilers
+    }
+}
