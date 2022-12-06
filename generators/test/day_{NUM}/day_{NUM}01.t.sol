a8e99604c158c4c2061873916374159462085f23
Clément Prévost <prevostclement@gmail.com>
2022-12-07 00:42:00 +0100

    Read test input from files

--- generators/test/day_{NUM}/day_{NUM}01.t.sol
+++ generators/test/day_{NUM}/day_{NUM}01.t.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/console2.sol";
-import "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import "../../src/day_{NUM}/Day{NUM}01.sol";
 
 contract Day{NUM}01Test is Test {
@@ -13,18 +12,15 @@ contract Day{NUM}01Test is Test {
     }
 
     function test_Day{NUM}01_Example1() public {
-        string memory input = "";
-        emit log_string(input);
+        string memory input = vm.readFile("./test/day_{NUM}/example.txt");
         {TANS} answer = day{NUM}.answer(input);
-        emit log_uint(answer);
         assertEq(answer, 15);
     }
 
     function test_Day{NUM}01_MainChallenge() public {
-        string memory input = "";
-        //emit log_string(input);
+        string memory input = vm.readFile("./test/day_{NUM}/main_input.txt");
         {TANS} answer = day{NUM}.answer(input);
-        console2.log("answer", answer);
+        console.log("answer", answer);
         assertEq(true, true); // no spoilers
     }
 }
