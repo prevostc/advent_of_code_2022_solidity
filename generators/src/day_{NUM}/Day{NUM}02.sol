36fba0d0cc84d22a6062b33f596915fa44bd4194
Clément Prévost <prevostclement@gmail.com>
2022-12-08 22:44:03 +0100

    Cleanup

--- generators/src/day_{NUM}/Day{NUM}02.sol
+++ generators/src/day_{NUM}/Day{NUM}02.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.13;
 
 import "forge-std/console2.sol";
 import {StringLib} from "../StringLib.sol";
+import {AsciiLib} from "../AsciiLib.sol";
 
 contract Day{NUM}02 {
     using StringLib for string;
+    using AsciiLib for string;
 
     function answer(string memory input) public view returns ({TANS}) {
         string[] memory lines = input.split("\n");
